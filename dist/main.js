fea5a9578e2fa26e2aee84bcbe8c0e16e6ffd867
skippharaoh <caniggiathompson@gmail.com>
2022-05-01 10:05:12 -0400

    Created A Function & Variable

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,7 @@
 var a = "1";
 console.log('aaa', a);
+var hello = "world";
+var getFullName = function (name, surname) {
+    return name + " " + surname;
+};
+console.log(getFullName("Skip", "Lessons"));
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 const a = "1";
-console.log('aaa', a)
+console.log('aaa', a);
+
+let hello: string = "world";
+
+const getFullName = (name: string, surname: string): string => {
+    return name + " " + surname
+}
+
+console.log(getFullName("Skip", "Lessons"))
