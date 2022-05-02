0277d4acd5932b3de3cec0d5243a710cf3d0da70
skippharaoh <caniggiathompson@gmail.com>
2022-05-02 16:51:24 -0400

    Classes & Generics

--- dist/main.js
+++ dist/main.js
@@ -13,6 +13,17 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 // Variables //
 var a = "1";
 console.log("aaa", a);
@@ -105,3 +116,17 @@ console.log(person.firstName);
 console.log(People.maxAge);
 var admin = new Admin('Foo', 'Bar');
 console.log(admin.getEditor);
+// Generics //
+var addId = function (obj) {
+    var id = Math.random().toString(16);
+    return __assign(__assign({}, obj), { id: id });
+};
+var some1 = {
+    name: 'Jack',
+    data: {
+        meta: 'foo'
+    }
+};
+// const result = addId<Some1Interface>(some1)
+// const result = addId<string>('foo')
+// console.log('result', result)
--- src/main.ts
+++ src/main.ts
@@ -147,3 +147,28 @@ console.log(People.maxAge)
 
 const admin = new Admin('Foo', 'Bar')
 console.log(admin.getEditor)
+
+// Generics //
+const addId = <T extends object>(obj: T) => {
+    const id: string = Math.random().toString(16)
+    return {
+        ...obj,
+        id
+    }
+}
+
+interface Some1Interface<T> {
+    name: string
+    data: T;
+}
+
+const some1: Some1Interface<{ meta: string }> = {
+    name: 'Jack',
+    data: {
+        meta: 'foo',
+    },
+}
+
+// const result = addId<Some1Interface>(some1)
+// const result = addId<string>('foo')
+// console.log('result', result)
